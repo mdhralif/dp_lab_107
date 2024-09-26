6fd8ea104f635ed9ce04d91b0572045cba884353
MD H R ALIF <122716402+mdhralif@users.noreply.github.com>
2024-09-26 23:30:04 +0600

    Delete RideSharingApp__Strategy_Pattern/src/CarpoolRide.java

--- RideSharingApp__Strategy_Pattern/src/CarpoolRide.java
+++
@@ -1,6 +0,0 @@
-public class CarpoolRide implements RideTypeStrategy {
-    @Override
-    public double calculateFare(double distance, double baseFare) {
-        return (baseFare + distance * 0.5) / 2;  // Carpool fare is split
-    }
-}
