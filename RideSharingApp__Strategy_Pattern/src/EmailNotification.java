21bb606469e3c618fb170e0574edeedaf51b84de
MD H R ALIF <122716402+mdhralif@users.noreply.github.com>
2024-09-26 23:30:25 +0600

    Delete RideSharingApp__Strategy_Pattern/src/EmailNotification.java

--- RideSharingApp__Strategy_Pattern/src/EmailNotification.java
+++
@@ -1,6 +0,0 @@
-public class EmailNotification implements NotificationStrategy {
-    @Override
-    public void sendNotification(String message) {
-        System.out.println("Sending Email notification: " + message);
-    }
-}
