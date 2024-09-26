78487e01343e95450603d924ed4580bf2ef665eb
MD H R ALIF <122716402+mdhralif@users.noreply.github.com>
2024-09-26 23:31:16 +0600

    Delete RideSharingApp__Strategy_Pattern/src/SMSNotification.java

--- RideSharingApp__Strategy_Pattern/src/SMSNotification.java
+++
@@ -1,6 +0,0 @@
-public class SMSNotification implements NotificationStrategy {
-    @Override
-    public void sendNotification(String message) {
-        System.out.println("Sending SMS notification: " + message);
-    }
-}
